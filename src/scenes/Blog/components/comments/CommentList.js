593cb3aafcbae36314b8e7e2e55266073788d4ce
wiliam969 <wiliamwallace969@googlemail.com>
2017-07-26 10:05:42 +0200

    w8 a minute ... we DID IT WE DIT IT WE DIT IT YEAH! comments are working ( get commentlist) we still cant post anything so

--- src/scenes/Blog/components/comments/CommentList.js
+++ src/scenes/Blog/components/comments/CommentList.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react'
 
 export default class CommentsList extends Component {
-
     render() {
-        console.log(this.props.comments)
             return (
                 <div className="container">
                     <h1>HELLO IM COMMENT LIST</h1>
-                    { this.props.comments > 0 ?
+                    { this.props.comments.length > 0 &&
                         this.props.comments.map((post,index) =>
-                            <div>{post.id}</div>
+                            <div>
+                                <p>{post.id}</p>
+                                <p>{post.author_name}</p>
+                                <p>{post.content.rendered}</p>
+                            </div>
                         )
-                        :
-                        <h1>I FUCKIN HATE U </h1>
                     }
-
-                    {/*{this.props.comments === 0 &&*/}
-                    {/*<div>WHY WE STILL HERE</div>*/}
-                    {/*}*/}
                 </div>
             )
     }
--- src/services/session/reducers/Comments.js
+++ src/services/session/reducers/Comments.js
@@ -1,31 +1,27 @@
 import { REQUEST_COMMENT,RECEIVE_COMMENT,INVALIDATE_COMMENT,SEND_COMMENT,FAILED_COMMENT } from '../actions/Comments'
 
-function Comments(state = {}, action) {
+function Comments(state = { didInvalidate: false, isFetching: false, comment: { id: 1}}, action) {
     switch(action.type) {
         case INVALIDATE_COMMENT:
             return Object.assign({}, state, {
                 didInvalidate: true,
                 isFetching:false,
-                comment: action.comment
             })
         case FAILED_COMMENT:
             return Object.assign({}, state, {
                 didInvalidate:true,
                 isFetching:false,
-                comment:action.comment
 
             })
         case REQUEST_COMMENT:
             return Object.assign({}, state, {
                 didInvalidate:false,
                 isFetching:false,
-                comment:[],
             })
         case SEND_COMMENT:
             return Object.assign({}, state, {
                 didInvalidate:false,
                 isFetching:false,
-                comment:action.comment,
             })
         case RECEIVE_COMMENT:
             return Object.assign({}, state, {

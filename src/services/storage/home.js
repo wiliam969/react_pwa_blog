9b772188b5583a203648707f750b07b64d836366
wiliam969 <wiliamwallace969@googlemail.com>
2017-07-27 11:07:04 +0200

    ok we accomplished rewrite the code like that so now we have a blog preview which loads one time the blogs via api. if you click on the update button a request send out to check for the next ones after that the items gets saved in a the database ez gg

--- src/services/storage/home.js
+++ src/services/storage/home.js
@@ -2,7 +2,7 @@ import db from '../storage/index'
 
 export default class HomeStorage {
     static getBlogPreview() {
-        return db.table('blog').limit(4).toArray().then(bitems => {
+        return db.table('blog').reverse().limit(4).toArray().then(bitems => {
             return bitems
         })
     }

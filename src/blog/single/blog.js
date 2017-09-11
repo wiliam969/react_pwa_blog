c5f8caea5e11a5884a95969da64cbff60edf2a6f
wiliam969 <wiliamwallace969@googlemail.com>
2017-09-11 12:00:50 +0200

    some css for the blog single stuff

--- src/blog/single/blog.js
+++ src/blog/single/blog.js
@@ -12,12 +12,14 @@ export default class BlogContent extends Component {
         return (
             <div>
                 {Object.keys(this.props.content).length > 0 &&
-                    <div className="blog-container">
+                    <div className="blog-single-container">
                         <Picture blogid={this.props.content.id} type="full" height="50vh" width="100%" backgroundsize="cover"></Picture>
-                        <div className="blog-content-wrapper container">
-                            <p className="blog-date">{new Date(this.props.content.date).toLocaleDateString()}</p>
-                            <div className="blog-title">{this.props.content.title.rendered}</div>
-                            <div className="blog-preview-text" dangerouslySetInnerHTML={{__html: this.props.content.content.rendered}}></div>
+                        <div className="blog-single-content-wrapper container">
+                            <div className="blog-single-content-wrapper-fix">
+                                <p className="blog-single-date">{new Date(this.props.content.date).toLocaleDateString()}</p>
+                                <div className="blog-single-title">{this.props.content.title.rendered}</div>
+                                <div className="blog-single-text" dangerouslySetInnerHTML={{__html: this.props.content.content.rendered}}></div>
+                            </div>
                         </div>
                     </div>
                 }

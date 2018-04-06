1b38daf09dc7c681efca1d8e39b103bf00eb9328
William <wiliam969@users.noreply.github.com>
2018-04-06 03:18:22 +0200

    kp

--- src/shared/slider/index.js
+++ src/shared/slider/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import ReactDOM from 'react-dom'
 import ReactSwipe from 'react-swipe'
 import {
     fetchSlider
@@ -16,38 +15,45 @@ import './slider.css'
 
 class Slider extends Component {
 
-    componentDidMount() {
-        const { dispatch } = this.props
-        dispatch(fetchSlider(this.props))
-    }
-
     humus = {
         height: 50 + 'vh',
         backgroundColor: 'green',
     }
 
+    componentDidMount() {
+        const { dispatch } = this.props
+        dispatch(fetchSlider(this.props))
+    }
+
+    next() {
+        return  this.reactSwipe.next()
+    }
+
+    prev() {
+        this.reactSwipe.prev()
+    }
+
     render () {
         return (
             <div className="slider-container-main">
-                <div className="slider-prev-btn">
-                    <Arrow type="left"  className="slider-prev-btn"></Arrow>
-                </div>
-
-                <div className="slider-next-btn">
-                    <Arrow type="right"  className="slider-prev-btn"></Arrow>
-                </div>
                 <div>
-                <ReactSwipe className="Slider" swipeOptions={{continuous: true, auto: 3000}} style={Style}>
-
+                    <ReactSwipe ref={reactSwipe => this.reactSwipe = reactSwipe} className="Slider" swipeOptions={{continuous: true }} style={Style}>
                         {this.props.Slider.items.length > 0 &&
                             this.props.Slider.items.map((post,index) =>
-                                <div className="slider-item" key={post.id} data-key={index} style={this.humus}>
+                                <div className="slider-item" key={index} style={this.humus}>
                                     <Picture posttype="slider" blogid={post.id} type="full" height="50vh" width="100%"></Picture>
-                                    <div className="slider-title">IM THE TITLE {index}</div>
+                                    <div className="slider-title">{index}</div>
                                 </div>
                             )
                         }
                 </ReactSwipe>
+                </div>
+                <div className="slider-prev-btn" onClick={this.prev}>
+                    <Arrow type="left"  className="slider-prev-btn"></Arrow>
+                </div>
+
+                <div className="slider-next-btn" onClick={this.next}>
+                    <Arrow type="right"  className="slider-prev-btn"></Arrow>
                 </div>
             </div>
         )

dbdc2fa2a38c4c0a70186b08b52059ca9b6b969a
William <wiliam969@users.noreply.github.com>
2018-04-18 00:51:50 +0200

    Menu is now really really cool =)

--- src/shared/Menu/index.js
+++ src/shared/Menu/index.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react'
-import { Link,NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './menu.css'
 import triangle from './triangle.svg'
 
-import { Col, Grid, Row} from 'react-bootstrap'
+import { Col, Grid, Row, Nav, Navbar, NavItem, Glyphicon} from 'react-bootstrap'
 
-import home from './home_black.png'
-import home_blue from './home_blue.png'
-import gallery from './gallery_black.png'
-import blog from './blog_black.png'
-import aboutme from './aboutme_black.png'
 
 export default class Menu extends Component {
     constructor(props) {
@@ -33,49 +28,59 @@ export default class Menu extends Component {
 
     render() {
         return (
-            <nav className="nav">
-                <div className="container">
-
-                <Grid>
-                    <Row className="show-grid">
-                        <Col xs={12} mdHidden={true} lgHidden={true} className="nav-menu-mobile">
-                            <NavLink exact className="nav-items-mobile" isActive={this.oddEvent} to="/">
-                                {this.oddEvent ?
-                                    <img src={home_blue} width={35} height={35} alt="home"/>
-                                    :
-                                    <img src={home} width={35} height={35} alt="home"/>
-                                }
-                            </NavLink>
-                            <NavLink className="nav-items-mobile" isActive={this.oddEvent} to="/blog/"><img src={blog} width={35} height={35} alt="blog"/></NavLink>
-                            <NavLink className="nav-items-mobile" isActive={this.oddEvent} to="/aboutme"><img src={aboutme} width={35} height={35} alt="gallery"/></NavLink>
-                            <NavLink className="nav-items-mobile" isActive={this.oddEvent} to="/gallery"><img src={gallery} width={35} height={35} alt="gallery"/></NavLink>
-                        </Col>
-                    </Row>
-                    <Row className="show-grid">
-                        <Col xs={12} xsHidden={true} smHidden={true} className="nav-menu-desktop">
-                            <Link className="nav-items" to="/">Home</Link>
-                            <Link className="nav-items" to="/blog/">Blog</Link>
-                            <Link className="nav-items" to="/aboutme">About Me</Link>
-                            <Link className="nav-items" to="/gallery">Gallery</Link>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <div className="triangle-down">
-                            <svg width="100%" height="100%" version="1.1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-                                <polyline className="triangle_polyline triangle" style={{fill: '#ffffff'}} points="50,0 0,100 100,100"></polyline>
-                                <polyline className="triangle_polyline left background" style={{fill:'transparent'}} points="100,0 50,0 100,100"></polyline>
-                                <polyline className="triangle_polyline right background" style={{fill:'transparent'}} points="50,0 0,0 0,100"></polyline>
-                            </svg>
-                        </div>
-                    </Row>
-                </Grid>
-
-
-
-
-
-                </div>
-            </nav>
+            <Grid>
+                <Row>
+                    <Col xs={12} mdHidden={true} lgHidden={true} className="nav-menu-mobile-col">
+                    <Navbar fixedTop={true} id="nav-menu-mobile-navbar">
+                        <Nav id="nav-menu-mobile-nav">
+                            <NavItem>
+                                <NavLink eventKey={1} exact className="nav-items-mobile" to="/">
+                                    <Glyphicon glyph="home" />
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink eventKey={2} className="nav-items-mobile"       to="/blog/">
+                                    <Glyphicon glyph="align-left" />
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink eventKey={3} className="nav-items-mobile"       to="/aboutme">
+                                    <Glyphicon glyph="user" />
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink eventKey={4} className="nav-items-mobile"       to="/gallery">
+                                    <Glyphicon glyph="picture" />
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Navbar>
+                    </Col>
+                    <Col xs={12} xsHidden={true} smHidden={true} className="nav-menu-desktop">
+                        <Navbar fixedTop={true} fluid={true} id="nav-menu-navbar">
+                            <Navbar.Header>
+                                <Navbar.Brand>
+                                    <NavLink className="nav-items" to="/">Kerstin Witte</NavLink>
+                                </Navbar.Brand>
+                            </Navbar.Header>
+                            <Nav id="nav-menu-nav">
+                                <NavItem>
+                                    <NavLink exact className="nav-items" to="/">Home</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-items" to="/blog/">Blog</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-items" to="/aboutme">About Me</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-items" to="/gallery">Gallery</NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Navbar>
+                    </Col>
+                </Row>
+            </Grid>
         );
     }
 }

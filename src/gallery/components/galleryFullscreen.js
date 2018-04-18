0cda60884616a19e368bf65cf3d1f050e0cbb596
wiliam969 <wiliamwallace969@googlemail.com>
2018-04-18 17:02:09 +0200

    fixing the margins on gallery

--- src/gallery/components/galleryFullscreen.js
+++ src/gallery/components/galleryFullscreen.js
@@ -24,7 +24,6 @@ export default class GalleryFullscreen extends Component {
     }
 
     /**
-     * Todo: Here i should use the Modal function of Bootstrap
      * @returns {*}
      */
     render() {
@@ -36,45 +35,40 @@ export default class GalleryFullscreen extends Component {
         const clapme = {}
         return (
             <div>
-                    { this.props.item.map((post,index) =>
-                    <Modal key={index} bsSize="large" show={this.props.isFullscreen} onHide={ (e) => this.closeButton(e,this.props)}>
-                            <Modal.Header closeButton>
-                                {post.title.rendered}
-                            </Modal.Header>
-                            <Modal.Body>
-                                <div style={{ width : "100%"}}>
-                                    <Picture
-                                        key={index}
-                                        featured_media_id={post.featured_media}
-                                        type="large"
-                                        posttype="gallery"
-                                        height="100vh"
-                                        width="100%"
-                                        backgroundSize="contain">
-                                    </Picture>
-                                </div>
-                            </Modal.Body>
+                { this.props.item.map((post,index) =>
+                <Modal key={index} bsSize="large" show={this.props.isFullscreen} onHide={ (e) => this.closeButton(e,this.props)}>
+                        <Modal.Header closeButton>
+                            {post.title.rendered}
+                        </Modal.Header>
+                        <Modal.Body>
+                            <div style={{ width : "100%"}}>
+                                <Picture
+                                    key={index}
+                                    featured_media_id={post.featured_media}
+                                    type="large"
+                                    posttype="gallery"
+                                    height="100vh"
+                                    width="100%"
+                                    backgroundSize="contain">
+                                </Picture>
+                            </div>
+                        </Modal.Body>
 
-                            <Modal.Footer>
-                                {!this.props.isURLFullscreen &&
-                                <div className='gal_fullscreen_btn_prev' onClick={ (e) => this.prevButton(e,this.props)} style={this.props.prev_state < 0 ? fuckme : clapme}>
-                                    <Arrow type="left"/>
-                                </div>
-                                }
-                                {!this.props.isURLFullscreen &&
-                                <div className='gal_fullscreen_btn_next' onClick={ (e) => this.nextButton(e,this.props)} style={this.props.last_item === this.props.next_state ? fuckme : clapme}>
-                                    <Arrow type="right" />
-                                </div>
-                                }
-                            </Modal.Footer>
+                        <Modal.Footer>
+                            {!this.props.isURLFullscreen &&
+                            <div className='gal_fullscreen_btn_prev' onClick={ (e) => this.prevButton(e,this.props)} style={this.props.prev_state < 0 ? fuckme : clapme}>
+                                <Arrow type="left"/>
+                            </div>
+                            }
+                            {!this.props.isURLFullscreen &&
+                            <div className='gal_fullscreen_btn_next' onClick={ (e) => this.nextButton(e,this.props)} style={this.props.last_item === this.props.next_state ? fuckme : clapme}>
+                                <Arrow type="right" />
+                            </div>
+                            }
+                        </Modal.Footer>
 
-                    </Modal>
-                    )}
-                {!this.props.isURLFullscreen &&
-                    <div className='gal_fullscreen_btn_next' onClick={ (e) => this.nextButton(e,this.props)} style={this.props.last_item === this.props.next_state ? fuckme : clapme}>
-                        <Arrow type="right" />
-                    </div>
-                }
+                </Modal>
+                )}
             </div>
         );
     }

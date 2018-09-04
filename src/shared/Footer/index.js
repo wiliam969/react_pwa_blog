75aba389fc6e937b7989bcc61b413ab01bd2017e
wiliam969 <wiliamwallace969@googlemail.com>
2018-09-04 11:58:13 +0200

    connected the impressum and the datenschutzerklärung with my footer

--- src/shared/Footer/index.js
+++ src/shared/Footer/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
 import './footer.css'
 
 import { Nav, Navbar, NavItem } from 'react-bootstrap'
@@ -11,19 +12,29 @@ export default class Footer extends Component {
     render() {
         return (
             <footer>
-                <Navbar fixedBottom={true} id="nav-menu-mobile-navbar">
-                    <Nav id="nav-menu-mobile-nav">
-                        <NavItem>
-                            <a href="mailto:admin@robin-witte.de"  target="_blank" rel="noopener noreferrer">
-                                <img src={mail} alt="mail"/>
-                            </a>
-                        </NavItem>
-                        <NavItem>
-                            <a href="https://github.com/wiliam969"  target="_blank" rel="noopener noreferrer">
-                                <img src={github} alt="github"/>
-                            </a>
-                        </NavItem>
-                    </Nav>
+                <Navbar fixedBottom={true} id="nav-menu-mobile-navbar" id="footer-menu">
+                    <Navbar.Collapse>
+                        <Nav id="icons">
+                            <NavItem>
+                                <a href="mailto:admin@robin-witte.de"  target="_blank" rel="noopener noreferrer">
+                                    <img src={mail} alt="mail"/>
+                                </a>
+                            </NavItem>
+                            <NavItem>
+                                <a href="https://github.com/wiliam969"  target="_blank" rel="noopener noreferrer">
+                                    <img src={github} alt="github"/>
+                                </a>
+                            </NavItem>
+                        </Nav>
+                        <Nav pullRight={true}>
+                            <NavItem className="nav-items">
+                                <NavLink to="/customsite/impressum">Impressum</NavLink>
+                            </NavItem>
+                            <NavItem className="nav-items">
+                                <NavLink to="/customsite/datenschutzerklaerung">Datenschutzerklärung</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Navbar>
             </footer>
         );
